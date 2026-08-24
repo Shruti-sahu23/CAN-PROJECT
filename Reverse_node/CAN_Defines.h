b5bae3eb66c818a4a54cf064a7d8a07dd4ecbf2e
Shruti-sahu <102422656+Shruti-sahu23@users.noreply.github.com>
2026-08-24 23:04:45 +0530

    define CAN 2.0B frame IDs and command constants
    Contains standard 11-bit CAN frame identifiers (0x101, 0x102, 0x103, 0x201), command opcodes, and obstacle distance threshold constants.

--- Reverse_node/CAN_Defines.h
+++ Reverse_node/CAN_Defines.h
@@ -1,10 +1,10 @@
 #ifndef __CAN_DEFINES_H__
 #define __CAN_DEFINES_H__
-
+ 
 /* CAN Message Identifiers */
 #define CAN_ID_REVERSE_ENABLE   0x102  /* Main Node -> Reverse Node */
 #define CAN_ID_DISTANCE_DATA    0x103  /* Reverse Node -> Main Node */
-
+ 
 /* Reverse Commands */
 #define REVERSE_DISABLE         0x00
 #define REVERSE_ENABLE          0x01
