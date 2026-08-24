e90481eda8aa04d368e7694dd1763274d885991c
Shruti-sahu <102422656+Shruti-sahu23@users.noreply.github.com>
2026-08-24 22:55:24 +0530

    add standard fixed-width typedefs and calibrated delay routines
    Provides ISO C99 fixed-width integer typedefs (u8, u16, u32, f32) and microsecond/millisecond ARM7 loop delays.

---
+++ Reverse_node/delays.c
@@ -0,0 +1,25 @@
+#include "delays.h"
+
+void delay_us(u32 us)
+{
+    u32 i;
+    for(; us > 0; us--)
+    {
+        for(i = 0; i < 10; i++)
+        {
+            __asm("NOP");
+        }
+    }
+}
+
+void delay_ms(u32 ms)
+{
+    u32 i;
+    for(; ms > 0; ms--)
+    {
+        for(i = 0; i < 10000; i++)
+        {
+            __asm("NOP");
+        }
+    }
+}
